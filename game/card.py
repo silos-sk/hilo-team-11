32a98fbc154e16bd97094b3ffa83401b60fe72d0
silos-sk <97029496+silos-sk@users.noreply.github.com>
2022-05-06 23:45:51 +0100

    text updates director-dealer card - self.card

--- game/card.py
+++ game/card.py
@@ -9,41 +9,41 @@ class Card:
         .    
     """
 
-def __init__(self):
-    """Constructs a new instance of Card
-    
-    Args:
-        self (Card): an instance of Card
-    """
+    def __init__(self):
+        """Constructs a new instance of Card
+        
+        Args:
+            self (Card): an instance of Card
+        """
 
-    self.value = 0
-    self.points = 0
-    self.previous_value = 0
-    self.guess = ''
-    
-
-
-    
-def draw(self):
-    """Generates a random value between 1 and 13 and calculates points based on player's guess.txt
-
-    Args:
-        self (Card): An instance of Card
-    
-    """
-
-    self.previous_value = random.randint(1, 13)
-    print(f'The card is {self.previous_value}')
-    guess = input('Higher or lower [h/l]')
-    self.value = random.randint(1, 13)
-    if self.value > self.previous_value and guess == 'h':
-        self.points = 100
-    elif self.value < self.previous_value and guess == 'l':
-        self.points = 100
-    elif sel.value > self.previous_value and guess == 'l':
-        self.points = -75
-    elif self.value < self.previous_value and guess == 'h':
-        self.points = -75
-    else:
+        self.value = 0
         self.points = 0
-    
+        self.previous_value = 0
+        self.guess = ''
+        
+
+
+        
+    def draw(self):
+        """Generates a random value between 1 and 13 and calculates points based on player's guess.txt
+
+        Args:
+            self (Card): An instance of Card
+        
+        """
+
+        self.previous_value = random.randint(1, 13)
+        print(f'The card is {self.previous_value}')
+        guess = input('Higher or lower [h/l]')
+        self.value = random.randint(1, 13)
+        if self.value > self.previous_value and guess == 'h':
+            self.points = 100
+        elif self.value < self.previous_value and guess == 'l':
+            self.points = 100
+        elif self.value > self.previous_value and guess == 'l':
+            self.points = -75
+        elif self.value < self.previous_value and guess == 'h':
+            self.points = -75
+        else:
+            self.points = 0
+        
